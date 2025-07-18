8753182eca305218c5c26cfa76e4f2daaad32df2
ooga518 <2401136@s.asojuku.ac.jp>
2025-07-18 10:46:39 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(function() {
     $('#btn').on('click', function(){
         $.ajax({
             //リクエスト先のURLを設定
-            url: "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $('#zipcode').val(),
+            url: "https://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $('#zipcode').val(),
             //レスポンスのデータ形式を設定
             dataType : 'jsonp',
         }).done(function(data){
